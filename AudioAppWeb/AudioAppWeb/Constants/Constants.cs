5f638db1600e05f8d3393bc088755a85d5292636
Selemabala <mabalaseleman@yahoo.com>
2025-04-25 13:05:25 +0200

    Introduction of the website
    Added introductory content and welcome message to homepage

---
+++ AudioAppWeb/AudioAppWeb/Constants/Constants.cs
@@ -0,0 +1,8 @@
+namespace AudioAppWeb.Constants;
+
+public static class Constants
+{
+    public  const string AppDeveloper = "Seleman";
+    public const string AppName = "AudioApp";
+    public const string Version = "1.0.0";
+}
