f2b3755d09eab9b9c9a0d674d87187be7fc2f71c
Seleman Pharles Mabala <Selemabala@github.com>
2025-12-18 16:31:47 +0300

    Registeration update
    This is the update of the changes made  to solve the problem which used to appear. An error message was appearing when one tries to register twice. Right now even if one tries to register as a completely new user nothing happens, no even an error message

--- AudioAppWeb/AudioAppWeb/Migrations/20251210184920_UpdateCustomerName.Designer.cs
+++ AudioAppWeb/AudioAppWeb/Migrations/20251210184920_UpdateCustomerName.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AudioAppWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AudioAppWeb.Migrations
 {
     [DbContext(typeof(AudioAppWebContext))]
-    partial class AudioAppWebContextModelSnapshot : ModelSnapshot
+    [Migration("20251210184920_UpdateCustomerName")]
+    partial class UpdateCustomerName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.6");
